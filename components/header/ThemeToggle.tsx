a540de934f8e456a6f9a502b4d830ff623396581
anthony <kimaniantoni@gmail.com>
2026-04-20 16:43:32 +0300

    fix: remove theme toggle dependency

--- components/header/ThemeToggle.tsx
+++ components/header/ThemeToggle.tsx
@@ -1,19 +1,12 @@
-'use client'
-import { Icon } from "@iconify/react/dist/iconify.js";
-import { useTheme } from "next-themes";
+'use client';
 
 const ThemeToggler = () => {
-  const { theme, setTheme } = useTheme();
   return (
     <button
       aria-label="theme toggler"
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-      className="group flex h-8 w-8 items-center justify-center duration-300"
+      className="flex h-8 w-8 items-center justify-center"
     >
-      <span className="group-hover:rotate-180 transition-transform duration-700 ease-in-out">
-        <Icon icon="ri:sun-fill" width="24" height="24" className="hidden dark:block"/>
-        <Icon icon="ri:moon-fill" width="24" height="24" className="dark:hidden" style={{ color: '#000' }} />
-      </span>
+      <span className="text-sm font-medium">A</span>
     </button>
   );
 };
